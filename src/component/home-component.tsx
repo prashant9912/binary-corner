2794dfff369833bc3a7b47493ffefa4eb32db0d5
prashant9912 <prashantg9912@gmail.com>
2024-05-19 13:10:21 +0530

    change title to blue

--- src/component/home-component.tsx
+++ src/component/home-component.tsx
@@ -28,7 +28,7 @@ export function HomeComponent() {
             />
           </svg>
         </a>
-        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-nonemd:text-5xl lg:text-6xl dark:text-white text-blue-500">
           Welcome to Binary Corner
         </h1>
         <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
